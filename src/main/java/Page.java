298d07cb6ed2eb553b2f15351c5ab3372dde4b54
PavelSavitskiy <Pavel_Savitskiy@epam.com>
2020-07-22 13:39:11 +0600

    Process

--- src/main/java/Page.java
+++ src/main/java/Page.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Page {
-    protected final  WebDriver driver;
+    private final  WebDriver driver;
 
     public Page(WebDriver driver) {
         this.driver = driver;
